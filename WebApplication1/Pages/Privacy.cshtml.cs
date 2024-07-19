cee4903fc8552d55a8617c8e437030d443dfc3a2
jackson-muema <devops@pycs.co.ke>
2024-07-19 07:56:13 +0300

    Add project files.

---
+++ WebApplication1/Pages/Privacy.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public string Message { get; set; }
+        //handle methods that respond to HTTP requests, e.x onGet, onPut, onPost
+        public void OnGet()
+        {
+            Message = "Hello world";
+        }
+    }
+
+}
